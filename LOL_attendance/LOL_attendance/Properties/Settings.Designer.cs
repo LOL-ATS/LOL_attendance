4ea36c898c0f90192847b868ebdaa4318540562f
Sviatlana Belabarodava <swetka_172006@mail.ru>
2018-03-02 15:26:18 +1300

    Revert "Add Sample DB"
    This reverts commit f145ddee0a4d882e4944df22d99f781ebafcc3a6.

--- LOL_attendance/LOL_attendance/Properties/Settings.Designer.cs
+++ LOL_attendance/LOL_attendance/Properties/Settings.Designer.cs
@@ -8,29 +8,22 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LOL_attendance.Properties {
-    
-    
+namespace LOL_attendance.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\LOL_DB.mdf;In" +
-            "tegrated Security=True;Connect Timeout=30")]
-        public string LOL_DBConnectionString {
-            get {
-                return ((string)(this["LOL_DBConnectionString"]));
             }
         }
     }
