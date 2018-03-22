8eeec44b0134cc5cce3cf2fc79c03eab9aac8250
Liana Pak <lianapaknz@gmail.com>
2018-03-22 18:07:46 +1300

    updateReject

--- LOL_attendance/LOL_attendance/TS.cs
+++ LOL_attendance/LOL_attendance/TS.cs
@@ -42,14 +42,13 @@ namespace LOL_attendance
 
             return result;
         }
-        public int UpdateSaved(string date, string hours, int employeeID, int siteID)
+        public int UpdateSavedOrRejected(string date, string hours, int employeeID, int siteID)
         {
             int result = 0;
             SqlConnection conn;
             SqlCommand cmd;
             conn = new SqlConnection(connStr);
-            cmd = new SqlCommand("INSERT into timesheet (date, hours, employee_id, site_id, status) VALUES (@date, @hours, @employee_id, @site_id,'Saved')", conn);
-            cmd = new SqlCommand("UPDATE timesheet SET hours = @hours where date = @date and employee_id = @employee_id and site_id = @site_id", conn);
+            cmd = new SqlCommand("UPDATE timesheet SET hours = @hours, status='Saved'  where date = @date and employee_id = @employee_id and site_id = @site_id", conn);
 
             cmd.Parameters.AddWithValue("@date", date);
 
